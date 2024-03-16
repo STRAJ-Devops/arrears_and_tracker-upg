a7384a1145ac7dd0cefdf085fd439f83d93aed11
unknown <Administrator@VUGA-001LGL006.visionfundug.com>
2024-03-16 08:09:29 +0300

    testing commit

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -21,8 +21,8 @@ class SaleController extends Controller
 
     public function group_by(Request $request)
     {
-        $logged_user = auth()->user()->user_type;
-        $staff_id = auth()->user()->staff_id;
+        $logged_user = 1;
+        $staff_id = 1;
         if ($request->has('group')) {
             if ($request->group == 'branches') {
                 //sales categorized by branches
