bfecadf03e03e8df5f1f2dc9d96c8e1ef8b3c963
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-07-11 11:59:37 +0300

    new update

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -11,19 +11,66 @@ class CustomerController extends Controller
     public function customer(Request $request)
     {
         $customer_id = $request->customer_id;
+        $search_by = $request->search_by;
 
-        //get customer name, phone, draw down balance, savings balance and loan balance
-        $customer_details = DB::table('customers')
-            ->join('arrears', 'customers.customer_id', '=', 'arrears.customer_id')
-            ->selectRaw('customers.names,
-                arrears.draw_down_balance,
-                arrears.savings_balance,
-                (arrears.outsanding_principal + arrears.real_outstanding_interest) as loan_balance,
-                (arrears.principal_arrears + arrears.outstanding_interest) as amount_due')
-            ->where('customers.customer_id', $customer_id)->first();
+        //check if search_by is customer_id, phone or name
 
-        if (!$customer_details) {
-            return response()->json(['message' => 'Customer not found'], 404);
+        if ($search_by == 'customer_id') {
+            $customer_details = DB::table('customers')
+                ->join('arrears', 'customers.customer_id', '=', 'arrears.customer_id')
+                ->selectRaw('
+                    customers.names,
+                    customers.phone,
+                    arrears.draw_down_balance,
+                    arrears.savings_balance,
+                    arrears.group_id,
+                    (arrears.outsanding_principal + arrears.real_outstanding_interest) as loan_balance,
+                    (arrears.principal_arrears + arrears.outstanding_interest) as amount_due')
+                ->where('customers.customer_id', $customer_id)
+                ->get();
+        } elseif ($search_by == 'phone') {
+            $customer_details = DB::table('customers')
+                ->join('arrears', 'customers.customer_id', '=', 'arrears.customer_id')
+                ->selectRaw('
+                    customers.names,
+                    customers.phone,
+                    arrears.draw_down_balance,
+                    arrears.savings_balance,
+                    arrears.group_id,
+                    (arrears.outsanding_principal + arrears.real_outstanding_interest) as loan_balance,
+                    (arrears.principal_arrears + arrears.outstanding_interest) as amount_due')
+                ->where('customers.phone', 'like', '%' . $customer_id . '%')
+                ->get();
+        } elseif ($search_by == 'name') {
+            $customer_details = DB::table('customers')
+                ->join('arrears', 'customers.customer_id', '=', 'arrears.customer_id')
+                ->selectRaw('
+                    customers.names,
+                    customers.phone,
+                    arrears.draw_down_balance,
+                    arrears.savings_balance,
+                    arrears.group_id,
+                    (arrears.outsanding_principal + arrears.real_outstanding_interest) as loan_balance,
+                    (arrears.principal_arrears + arrears.outstanding_interest) as amount_due')
+                ->where('customers.names', 'like', '%' . $customer_id . '%')
+                ->get();
+        } else if ($search_by == 'group_id') {
+
+            $customer_details = DB::table('customers')
+                ->join('arrears', 'customers.customer_id', '=', 'arrears.customer_id')
+                ->selectRaw('
+                    customers.names,
+                    customers.phone,
+                    arrears.draw_down_balance,
+                    arrears.savings_balance,
+                    arrears.group_id,
+                    (arrears.outsanding_principal + arrears.real_outstanding_interest) as loan_balance,
+                    (arrears.principal_arrears + arrears.outstanding_interest) as amount_due')
+                ->where('arrears.group_id', $customer_id)
+                ->get();
+
+        } else {
+            return response()->json(['message' => 'Invalid search_by parameter'], 400);
         }
 
         return response()->json($customer_details, 200);
