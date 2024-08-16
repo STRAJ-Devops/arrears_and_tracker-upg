59d9c933c5d949a15b862476f0c43dbb91dc9623
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-16 19:19:09 +0300

    new upload

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -28,6 +28,10 @@ class CalendarController extends Controller
         $event_data = [];
 
         foreach ($arrear_groups as $arrear) {
+            // If next_repayment_date is empty, set it to today
+            if ($arrear->next_repayment_date == "") {
+continue;
+}
             // Convert the next_repayment_date to "Y-m-d"
             $next_repayment_date = date('Y-m-d', strtotime($arrear->next_repayment_date));
 
