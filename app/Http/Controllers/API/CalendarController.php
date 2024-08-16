c3920b0c62b7e527d23f285a901b15b2d529bbe8
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-16 14:14:03 +0300

    new update

--- app/Http/Controllers/API/CalendarController.php
+++ app/Http/Controllers/API/CalendarController.php
@@ -6,61 +6,47 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Arrear;
 use Illuminate\Support\Facades\DB;
-
 class CalendarController extends Controller
 {
     public function getcalender(Request $request)
     {
         $events = [];
 
+        // Get arrear groups from the database
         $arrear_groups = Arrear::where('lending_type', 'Group')
             ->groupBy('group_id', 'next_repayment_date')
             ->select('group_id', 'next_repayment_date', DB::raw('count(*) as group_count'))
             ->get();
 
+        // Create an array to aggregate group counts by next_repayment_date
+        $event_data = [];
+
         foreach ($arrear_groups as $arrear) {
-            //if $arrear->next_repayment_date is "", set it to today
+            // If next_repayment_date is empty, set it to today
             if ($arrear->next_repayment_date == "") {
                 $arrear->next_repayment_date = date('Y-m-d');
             }
-            //convert date  and in "2024-11-12"
+            // Convert the next_repayment_date to "Y-m-d"
             $next_repayment_date = date('Y-m-d', strtotime($arrear->next_repayment_date));
-            //then convert it back to string
 
-            //check if the start same as the repayment date already exists
-            $exists = false;
-            foreach ($events as $event) {
-                if ($event['start'] == $next_repayment_date) {
-                    $exists = true;
-                    break;
-                }
+            // Aggregate group counts by next_repayment_date
+            if (!isset($event_data[$next_repayment_date])) {
+                $event_data[$next_repayment_date] = 0;
             }
+            $event_data[$next_repayment_date] += $arrear->group_count;
+        }
 
-            if ($exists) {
-                //just sum the group count
-                foreach ($events as $key => $event) {
-                    if ($event['start'] == $next_repayment_date) {
-                        //get new group count
-                        $group_count = $arrear->group_count + $event['group_count'];
-
-                        //update the group count
-                        $events[$key]['group_count'] = $group_count;
-
-                        //update the title
-                        $events[$key]['title'] = $group_count . ' Group(s) to repay';
-                    }
-                }
-            } else {
+        // Convert aggregated data into events
+        foreach ($event_data as $date => $group_count) {
             array_push($events, [
-                'title' => $arrear->group_count . ' Group(s) to repay',
-                'start' => $next_repayment_date,
-                'end' => $next_repayment_date,
+                'title' => $group_count . ' Group(s) to repay',
+                'start' => $date,
+                'end' => $date,
                 'className' => 'bg-warning',
-                'group_count' => $arrear->group_count
             ]);
         }
-        }
-        return response()->json(["status" => true, "data" => $events]);
 
+        return response()->json(["status" => true, "data" => $events]);
     }
+
 }
