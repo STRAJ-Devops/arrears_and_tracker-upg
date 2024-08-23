5d38ced3000c4c0ee393d7497608704833fb86d8
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-23 11:44:35 +0300

    new update

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /var/www/vfuarrearsandtracker.impact-outsourcing.com || exit
-git pull origin main >> /var/www/logs/deploy.log 2>&1
-composer install --optimize-autoloader --no-dev >> /var/www/storage/deploy.log 2>&1
-php artisan migrate --force >> /var/www/logs/deploy.log 2>&1
-php artisan config:cache >> /var/www/logs/deploy.log 2>&1
-php artisan route:cache >> /var/www/logs/deploy.log 2>&1
-php artisan view:cache >> /var/www/logs/deploy.log 2>&1
+git pull origin main >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage 2>&1
+composer install --optimize-autoloader --no-dev >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
+php artisan migrate --force >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log 2>&1
+php artisan config:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
+php artisan route:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
+php artisan view:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
