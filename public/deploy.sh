681ada80bec91deb2cc69624f12eb1b316420039
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-23 12:10:53 +0300

    new updaate

--- public/deploy.sh
+++ public/deploy.sh
@@ -4,6 +4,10 @@ cd /var/www/vfuarrearsandtracker.impact-outsourcing.com || exit
 git pull origin master >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
 composer install --optimize-autoloader --no-dev >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
 php artisan migrate --force >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log 2>&1
-php artisan config:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
-php artisan route:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
-php artisan view:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/laravel.log 2>&1
+php artisan config:cache >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log 2>&1
+# compile assets with npm
+npm install >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log 2>&1
+# build assets
+npm run build >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log 2>&1
+# log a  deployment success message
+echo "Deployment successful at $(date)" >> /var/www/vfuarrearsandtracker.impact-outsourcing.com/storage/logs/laravel.log
