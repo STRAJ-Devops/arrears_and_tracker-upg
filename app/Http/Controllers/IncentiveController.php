947ec7b783a4544bbafaa53ddc567ad68fe78f93
Tunde <tundetimah150t@gmail.com>
2025-08-30 12:46:41 +0000

    UPDATE

--- app/Http/Controllers/IncentiveController.php
+++ app/Http/Controllers/IncentiveController.php
@@ -435,6 +435,18 @@ class IncentiveController extends Controller
         $retentionRatio = round(($currentClients / $denominator), 2);
         // Log::debug("Retention ratio for staff_id {$staffId}: {$retentionRatio}");
 
+        Log::debug("Retention Calculation", [
+            'staff_id' => $staffId,
+            'currentClients' => $currentClients,
+            'previousClients' => $previousClients,
+            'newCycle1Clients' => $newCycle1Clients,
+            'denominator' => $denominator,
+            'retentionRatio' => ($denominator === 0 ? 0 : round(($currentClients / $denominator), 2)),
+            'month' => $currentMonth,
+            'lendingType' => $lendingType,
+        ]);
+        
+
         return $retentionRatio;
     }
 
@@ -461,15 +473,15 @@ class IncentiveController extends Controller
         $retentionScore = round((($actualRetention - $min) / ($max - $min)) * ($weight / 100) * $maximumIncentive, 2);
 
 
-        // Log::debug("calculateRetentionScore inputs:", [
-        //     'lendingType' => $lendingType,
-        //     'actualRetention' => $actualRetention,
-        //     'minThreshold' => $min,
-        //     'maxThreshold' => $max,
-        //     'weightPercent' => $weight,
-        //     'retentionScore' => $retentionScore,
-        //     'maxIncentive' => $maximumIncentive,
-        // ]);
+        Log::debug("calculateRetentionScore inputs:", [
+            'lendingType' => $lendingType,
+            'actualRetention' => $actualRetention,
+            'minThreshold' => $min,
+            'maxThreshold' => $max,
+            'weightPercent' => $weight,
+            'retentionScore' => $retentionScore,
+            'maxIncentive' => $maximumIncentive,
+        ]);
 
         return round($retentionScore, 2);
     }
